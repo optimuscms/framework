9aedc723351e70872dfc76a56bbb9fbfc1eefa89
Jack Robertson <jack@optixsolutions.co.uk>
2020-02-21 15:21:32 +0000

    Temporarily remove tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,49 +2,9 @@
 
 namespace OptimusCMS\Tests;
 
-use OptimusCMS\Users\Models\AdminUser;
-use OptimusCMS\Meta\MetaServiceProvider;
-use OptimusCMS\Pages\PageServiceProvider;
-use OptimusCMS\Users\UserServiceProvider;
-use OptimusCMS\Media\MediaServiceProvider;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->withFactories(__DIR__.'/factories');
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            MediaServiceProvider::class,
-            MetaServiceProvider::class,
-            PageServiceProvider::class,
-            UserServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-    }
-
-    protected function signIn(Authenticatable $user = null)
-    {
-        $user = $user ?: factory(AdminUser::class)->create();
-
-        $this->actingAs($user, 'admin');
-
-        return $user;
-    }
+    //
 }
