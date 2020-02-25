6198e71473a59ab9bf6d9039fd08c8eb5176de41
Jack Robertson <jcrobertson97@gmail.com>
2020-02-25 19:40:40 +0000

    Docblock types and other small refactors

--- src/Pages/PageTemplates.php
+++ src/Pages/PageTemplates.php
@@ -7,26 +7,78 @@ use OptimusCMS\Pages\Contracts\PageTemplate;
 
 class PageTemplates
 {
+    /** @var array */
     protected static $templates = [];
 
+    /**
+     * @param mixed $templates
+     * @return void
+     */
     public static function register($templates)
     {
         if (is_array($templates)) {
             return self::registerMany($templates);
         }
 
-        if (
-            is_string($templates)
-            || $templates instanceof PageTemplate
-        ) {
-            return self::registerOne($templates);
-        }
+        self::registerOne($templates);
+    }
 
-        throw new InvalidArgumentException(
-            'The given page template type is invalid.'
+    /**
+     * @return array
+     */
+    public static function getAll()
+    {
+        return array_values(self::$templates);
+    }
+
+    /**
+     * @return array
+     */
+    public static function loadAll()
+    {
+        return array_map(
+            function ($template) {
+                return self::resolveFromContainer($template);
+            },
+            self::getAll()
         );
     }
 
+    /**
+     * @param string $id
+     * @return string
+     */
+    public static function get(string $id)
+    {
+        if (! self::exists($id)) {
+            throw new InvalidArgumentException();
+        }
+
+        return self::$templates[$id];
+    }
+
+    /**
+     * @param string $id
+     * @return PageTemplate
+     */
+    public static function load(string $id)
+    {
+        return self::resolveFromContainer(self::get($id));
+    }
+
+    /**
+     * @param string $id
+     * @return bool
+     */
+    public static function exists(string $id)
+    {
+        return isset(self::$templates[$id]);
+    }
+
+    /**
+     * @param array $templates
+     * @return void
+     */
     protected static function registerMany(array $templates)
     {
         foreach ($templates as $template) {
@@ -34,59 +86,40 @@ class PageTemplates
         }
     }
 
+    /**
+     * @param mixed $template
+     * @return void
+     */
     protected static function registerOne($template)
     {
+        if (! self::verify($template)) {
+            throw new InvalidArgumentException();
+        }
+
         if ($template instanceof PageTemplate) {
             $template = get_class($template);
-        } elseif (
-            ! is_string($template)
-            || ! is_subclass_of($template, PageTemplate::class, true)
-        ) {
-            throw new InvalidArgumentException(
-                'The given page template type is invalid.'
-            );
         }
 
         self::$templates[$template::getId()] = $template;
     }
 
-    public static function getAll()
+    /**
+     * @param string $template
+     * @return bool
+     */
+    protected static function verify($template)
     {
-        return array_values(self::$templates);
-    }
-
-    public static function loadAll()
-    {
-        $templates = [];
-
-        foreach (self::getAll() as $template) {
-            $templates[] = self::resolveFromContainer($template);
-        }
-
-        return $templates;
-    }
-
-    public static function get(string $id)
-    {
-        if (! self::exists($id)) {
-            throw new InvalidArgumentException(
-                "A page template with the id [{$id}] has not been registered."
+        return $template instanceof PageTemplate
+            || (
+                is_string($template)
+                && is_subclass_of($template, PageTemplate::class, true)
             );
-        }
-
-        return self::$templates[$id];
     }
 
-    public static function load($id)
-    {
-        return self::resolveFromContainer(self::get($id));
-    }
-
-    public static function exists(string $id)
-    {
-        return isset(self::$templates[$id]);
-    }
-
+    /**
+     * @param string $template
+     * @return PageTemplate
+     */
     protected static function resolveFromContainer(string $template)
     {
         return app()->get($template);
