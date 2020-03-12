9c554b50fcddcb63dff44074630478e0df556230
Jack Robertson <jack@optixsolutions.co.uk>
2020-03-12 15:59:22 +0000

    Add page model unit tests

--- tests/Pages/PageTest.php
+++ tests/Pages/PageTest.php
@@ -2,7 +2,50 @@
 
 namespace OptimusCMS\Tests\Pages;
 
+use OptimusCMS\Pages\Models\Page;
+
 class PageTest extends TestCase
 {
-    //
+    /** @test */
+    public function it_can_build_a_path()
+    {
+        $rootPage = new Page();
+        $rootPage->forceFill([
+            'path' => 'root',
+        ]);
+
+        $parentPage = new Page();
+        $parentPage->parent()->associate($rootPage);
+        $parentPage->forceFill([
+            'path' => 'root/parent',
+        ]);
+
+        $page = new Page();
+        $page->parent()->associate($parentPage);
+        $page->forceFill([
+            'slug' => 'current',
+            'path' => 'existing/path',
+        ]);
+
+        $this->assertEquals('root/parent/current', $page->buildPath());
+    }
+
+    /** @test */
+    public function it_will_not_build_a_new_path_if_its_fixed()
+    {
+        $parentPage = new Page();
+        $parentPage->forceFill([
+            'path' => 'parent',
+        ]);
+
+        $page = new Page();
+        $page->parent()->associate($parentPage);
+        $page->forceFill([
+            'slug' => 'current',
+            'path' => 'existing/path',
+            'has_fixed_path' => true,
+        ]);
+
+        $this->assertEquals('existing/path', $page->buildPath());
+    }
 }
